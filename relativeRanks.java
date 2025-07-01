7caf580640410158519dada443b21baf8e9064a0
atulyado <133553524+atulyado@users.noreply.github.com>
2025-07-01 19:35:11 +0700

    Update relativeRanks.java

--- relativeRanks.java
+++ relativeRanks.java
@@ -4,13 +4,13 @@ class Solution {
         String[] result = new String[n];
         Integer[] indices = new Integer[n];
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){// Create an array of indexes 0...n-1
             indices[i] = i;
         }
 
-        Arrays.sort(indices, (a,b) -> score[b] - score[a]);
+        Arrays.sort(indices, (a,b) -> score[b] - score[a]);// Sort indices based on score values in descending order
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){// Assign medals and ranks
         if(i == 0){
             result[indices[i]] = "Gold Medal";
         }else if(i == 1){
